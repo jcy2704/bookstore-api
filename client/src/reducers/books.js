58ade48fbdad3d5a8e174432455079cc2f55b83e
Steven J Chung <stevenjchung12@gmail.com>
2021-03-30 13:23:49 -0500

    Remove unnecessary code

--- client/src/reducers/books.js
+++ client/src/reducers/books.js
@@ -1,19 +1,13 @@
 /* eslint-disable max-len */
 import { CREATE_BOOK, REMOVE_BOOK, LOAD_BOOKS } from '../actions/index';
 
-export const randInt = (minim, maxim) => {
-  const min = Math.ceil(minim);
-  const max = Math.floor(maxim);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
 const bookReducer = (state = [], action) => {
   switch (action.type) {
     case LOAD_BOOKS:
       return action.books;
     case CREATE_BOOK:
       return [
-        ...state, { title: action.book.title, category: action.book.category },
+        ...state, { title: action.book.title, category: action.book.category, author: action.book.author },
       ];
     case REMOVE_BOOK:
       return state.filter(book => book.id !== action.book.id);
